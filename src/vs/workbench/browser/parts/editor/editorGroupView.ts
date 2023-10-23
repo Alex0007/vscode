91fd293ab0c94c04984ccca8eb7c1e5748f7bcb6
ermin.zem <harbin1053020115@gmail.com>
2023-10-23 18:14:58 +0800

    fix: fix editor progress position when enabled pinnedTabsOnSeparateRow (#195314)
    * fix: fix editor progress position when enabled pinnedTabsOnSeparateRow
    
    * fix: fix editor progress position
    
    * fix: fix editor progress position
    
    * fix: add commit to editor title progress style
    
    * Revert "fix: add commit to editor title progress style"
    
    This reverts commit 302ec88c9ad48074669b5abf97fd294549bceb24.
    
    * Revert "fix: fix editor progress position"
    
    This reverts commit ec445b607cd0b64febe84b14b3cb261ea32f9c0c.
    
    * Revert "fix: fix editor progress position"
    
    This reverts commit cea64becfa492826069486462df5287dafb78aa2.
    
    * Revert "fix: fix editor progress position when enabled pinnedTabsOnSeparateRow"
    
    This reverts commit 8f2c0e702205dbd04599605b86db5f872d61473a.
    
    * feat: add --editor-group-tabs-height css variable to set progress bar position
    
    * update height in lyout
    
    * Remove setting redraws as already done elsewhere
    
    * Remove 2px for progress bit height
    
    ---------
    
    Co-authored-by: ermin.zem <ermin.zem@alibaba-inc.com>
    Co-authored-by: BeniBenj <besimmonds@microsoft.com>

--- src/vs/workbench/browser/parts/editor/editorGroupView.ts
+++ src/vs/workbench/browser/parts/editor/editorGroupView.ts
@@ -1939,6 +1939,8 @@ export class EditorGroupView extends Themable implements IEditorGroupView {
 			available: new Dimension(width, height - this.editorPane.minimumHeight)
 		});
 
+		this.element.style.setProperty('--editor-group-tabs-height', `${this.titleHeight.offset}px`);
+
 		// Pass the container width and remaining height to the editor layout
 		const editorHeight = Math.max(0, height - titleControlSize.height);
 		this.editorContainer.style.height = `${editorHeight}px`;
